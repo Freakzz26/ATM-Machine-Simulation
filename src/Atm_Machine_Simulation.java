64bd7cd02a70b91752657c6eff685fa9c6b206da
Freakzz26 <madhumithamurali02@gmail.com>
2023-07-11 19:27:21 +0530

    added some changes

--- src/Atm_Machine_Simulation.java
+++ src/Atm_Machine_Simulation.java
@@ -15,14 +15,15 @@ class InvalidAmoutException extends Exception
 }
 class Balance
 {
-    private int account_balance=50000;
+    private int account_balance=500;
 
     public int getAccount_balance() {
         return account_balance;
     }
 
-    public void setAccount_balance(int account_balance) {
+    public int setAccount_balance(int account_balance) {
         this.account_balance = account_balance;
+        return account_balance;
     }
 }
 public class Atm_Machine_Simulation  {
@@ -38,49 +39,50 @@ public class Atm_Machine_Simulation  {
         else
         {
             System.out.println("Your money withdrawed successfully");
-            System.out.println("Your current balance is: "+Math.abs(account_balance-withdraw));
+
+            System.out.println("Your current balance is: "+Math.abs(account_balance));
         }
 
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Do you wish to a) Withdraw or b) Deposit or c)Check Balance or d) Exit? ");
-        String ch= scanner.next();
-        int deposit=0;
+        String ch;
+        int deposit=0,withdraw=0;
         Balance balance=new Balance();
-        if(ch.equals("a"))
-        {
-            System.out.println("Enter the amount to be withdrawed: ");
-            int withdraw=scanner.nextInt();
-            try
-            {
-                validateFunds(withdraw,balance.getAccount_balance());
-            }
-            catch(InsufficientFundsException exception)
-            {
-                System.out.println(exception.getMessage());
-            }
-            catch (InvalidAmoutException exception)
-            {
-                System.out.println(exception.getMessage());
-            }
+        int x=0,y=0;
+        do {
+            System.out.println("Do you wish to a) Withdraw or b) Deposit or c)Check Balance or d) Exit? ");
+            ch= scanner.next();
+            if (ch.equals("a")) {
+                System.out.println("Enter the amount to be withdrawed: ");
+                withdraw = scanner.nextInt();
+                try {
+                    x=balance.setAccount_balance(balance.getAccount_balance()-withdraw);
+                    validateFunds(withdraw,x);
+                } catch (InsufficientFundsException exception) {
+                    System.out.println(exception.getMessage());
+                } catch (InvalidAmoutException exception) {
+                    System.out.println(exception.getMessage());
+                }
 
-        }
-        else if(ch.equals("b"))
+            } else if (ch.equals("b")) {
+                System.out.println("Enter the amount to be deposited: ");
+                deposit = scanner.nextInt();
+                balance.setAccount_balance(balance.getAccount_balance()+deposit);
+                System.out.println("Your Balance is "+balance.getAccount_balance());
+            } else if (ch.equals("c")) {
+                System.out.println("Your Current Balance is: " + balance.getAccount_balance());
+            }
+            else if(ch.equals("d"))
+            {
+                break;
+            }
+        }while(!ch.equals("d"));
+        if(ch.equals("d"))
         {
-            System.out.println("Enter the amount to be deposited: ");
-            deposit=scanner.nextInt();
-            System.out.println("Money Deposited!");
-            System.out.println("YOUR CURRENT BALANCE: "+(deposit+balance.getAccount_balance()));
+            System.out.println("Thankyou!!");
         }
-        else if(ch.equals("c"))
-        {
-            System.out.println("Your Current Balance is: "+(deposit+balance.getAccount_balance()));
-        }
-        else if(ch.equals("d"))
-        {
-            System.out.println("Exit");
-        }
+
 
     }
 }
